a8d35074fbd83f418fe249fc29c3ea55cf53cd33
Matthieu Nedir <matthieunedir@orange.fr>
2022-12-28 17:43:59 +0100

    Routing #6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthPageComponent } from './auth-page/auth-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'auth',
+   children: [
+    {path: 'auth-page', component: AuthPageComponent}
+   ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
