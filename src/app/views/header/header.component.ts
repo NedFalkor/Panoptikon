c9c1030f8e1797e3757a5d57620efdadf1301c70
Matthieu Nedir <matthieunedir@orange.fr>
2023-01-15 09:24:05 +0100

    ajout icons

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
+
+  @Input()
+  selected: string = ''; 
+  
+  @Output()
+  onClose = new EventEmitter();
+
+  showModal: boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  cancel() { this.onClose.emit(null); }
+  showSearch() { this.onClose.emit(this.selected) }
+
+  showSearchBar() {
+    this.showModal = true;
+
+  }
 }
