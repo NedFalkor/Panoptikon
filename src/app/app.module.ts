dcce542022212bb1a0b0fc65fc888d93ad84c14f
Matthieu Nedir <matthieunedir@orange.fr>
2023-03-18 10:06:05 +0100

    Ajout de modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './views/header/header.component';
-import { FooterComponent } from './views/footer/footer.component';
-import { MainMenuComponent } from './views/main-menu/main-menu.component';
-import { AccountChannelComponent } from './views/account/account-channel/account-channel.component';
-import { SearchPageComponent } from './views/search/search-page/search-page.component';
-import { AddVideoComponent } from './views/add/add-video/add-video.component';
-import { VideoViewComponent } from './views/video-view/video-view.component';
-import { RegisterComponent } from './views/register/register.component';
-import { SubscriptionComponent } from './views/subscription/subscription.component';
-import { AddUserComponent } from './views/add/add-user/add-user.component';
-import { FilterPipe } from './views/add/add-user/filterUser.pipe';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { AccountChannelComponent } from './components/account/account-channel/account-channel.component';
+import { SearchPageComponent } from './components/search/search-page/search-page.component';
+import { AddVideoComponent } from './components/add/add-video/add-video.component';
+import { VideoViewComponent } from './components/video-view/video-view.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SubscriptionComponent } from './components/subscription/subscription.component';
+import { AddUserComponent } from './components/add/add-user/add-user.component';
+import { FilterPipe } from './components/add/add-user/filterUser.pipe';
 import { WebcamModule } from 'ngx-webcam';
-import { CaptureImageComponent } from './views/captureMedia/capture-image/capture-image.component';
+import { CaptureImageComponent } from './components/account/account-settings/capture-image/capture-image.component';
+import { VideoPLayerComponent } from './components/video-player/video-player.component';
 
 
 @NgModule({
@@ -35,7 +36,8 @@ import { CaptureImageComponent } from './views/captureMedia/capture-image/captur
     SubscriptionComponent,
     AddUserComponent,
     FilterPipe,
-    CaptureImageComponent
+    CaptureImageComponent,
+    VideoPLayerComponent
 
   ],
   imports: [
---
+++ src/app/components/add/add-video/add-video.component.ts
@@ -0,0 +1,43 @@
+import { Component, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-add-video',
+  templateUrl: './add-video.component.html',
+  styleUrls: ['./add-video.component.scss']
+})
+
+export class AddVideoComponent {
+
+  queryParams: any;
+
+  @ViewChild('videoForm') form: HTMLFormElement;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.queryParams = params;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      let data = new FormData(this.form);
+
+      fetch('http://localhost:8080/video', {
+        method: 'POST',
+        body: data
+      })
+        .then(result => result.text())
+        .then(_ => {
+          window.location.reload();
+        });
+    });
+    // get the video file from the form
+    const file = this.form.querySelector('input[type="file"]').files[0];
+    const fileURL = URL.createObjectURL(file);
+  }
+}
