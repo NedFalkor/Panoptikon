92e707f15cb23f754428810a92ac602e900417db
Matthieu Nedir <matthieunedir@orange.fr>
2023-03-26 16:02:03 +0200

    implémentation composant + style

--- src/app/components/video-view/video-view.component.ts
+++ src/app/components/video-view/video-view.component.ts
@@ -1,27 +1,45 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-video-view',
   templateUrl: './video-view.component.html',
-  styleUrls: ['./video-view.component.scss']
+  styleUrls: ['./video-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class VideoViewComponent implements OnInit {
 
-  queryParams: any;
-  @ViewChild('videoScreen') videoScreen: ElementRef<HTMLVideoElement>;
-  @ViewChild('videoDiv') videoDiv: ElementRef<HTMLDivElement>;
+export class VideoViewComponent implements OnInit, OnDestroy {
+
+  queryParams: any = {};
+
+  videoSrc: string = '';
+
+  @ViewChild('videoScreen', { static: true }) videoScreen!: ElementRef<HTMLVideoElement>;
+  @ViewChild('videoDiv', { static: true }) videoDiv!: ElementRef<HTMLDivElement>;
+
+  private subscription!: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe(params => {
+    this.subscription = this.activatedRoute.queryParams.subscribe(params => {
       this.queryParams = params;
       if (this.queryParams.video) {
-        this.videoScreen.nativeElement.src = "http://localhost:8080/video/${this.queryParams.video}";
+        this.videoSrc = `http://localhost:8080/video/${this.queryParams.video}`;
+        this.videoScreen.nativeElement.src = this.videoSrc;
         this.videoDiv.nativeElement.style.display = 'block';
-        document.querySelector('#now-playing').innerText = 'Now playing ' + this.queryParams.video;
+        const nowPlaying = document.querySelector('#now-playing');
+        if (nowPlaying) {
+          nowPlaying.textContent = 'Now playing ' + this.queryParams.video;
+        }
       }
     });
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
+
