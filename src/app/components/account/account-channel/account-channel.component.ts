7e2a633a1988c8afbe5243567bc8fb77cd81dd22
Matthieu Nedir <matthieunedir@orange.fr>
2023-04-02 16:24:26 +0200

    ajout header

--- src/app/components/account/account-channel/account-channel.component.ts
+++ src/app/components/account/account-channel/account-channel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, Input } from '@angular/core';
 
 @Component({
   selector: 'app-account-channel',
@@ -6,12 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-channel.component.scss']
 })
 export class AccountChannelComponent implements OnInit {
-  
-videos: any;
+
+  @ViewChild('headerTemplate', { static: true }) headerTemplate: TemplateRef<any>;
+
+  @Input() leftButton!: TemplateRef<any>;
+  @Input() rightButton!: TemplateRef<any>;
+
+  videos: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.leftButton = `
+      <a class="button is-outlined is-white is-focused is-hovered has-text-black my-1 mx-2 p-1" routerLink="/menu">
+        <span class="icon">
+          <i class="fas fa-bars"></i>
+        </span>
+        Menu
+      </a>
+    `;
+    this.rightButton = `
+      <a class="button is-outlined is-link is-focused is-hovered has-text-white my-1 mx-2 p-1" routerLink="/settings">
+        Paramètres
+      </a>
+    `;
   }
 
 }
