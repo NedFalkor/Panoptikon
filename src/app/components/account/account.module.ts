caaa21c915dc0dcf2bc3329f1f529885d1632546
Matthieu Nedir <matthieunedir@orange.fr>
2023-04-02 18:50:13 +0200

    suppression module

--- src/app/components/account/account.module.ts
+++ src/app/components/account/account.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CaptureImageComponent } from './capture-image/capture-image.component';
+import { WebcamModule } from 'ngx-webcam';
+
 
 @NgModule({
   declarations: [
