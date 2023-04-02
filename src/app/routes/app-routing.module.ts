4ca8171b29519b7766e25e2a36fbc488ffd10c6f
Matthieu Nedir <matthieunedir@orange.fr>
2023-04-02 15:14:08 +0200

    ajout de routing

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -5,6 +5,7 @@ import { MainMenuComponent } from '../components/main-menu/main-menu.component';
 import { SearchPageComponent } from '../components/search/search-page/search-page.component';
 import { AddUserComponent } from '../components/add/add-user/add-user.component';
 import { RegisterComponent } from '../components/register/register.component';
+import { SubscriptionComponent } from '../components/subscription/subscription.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent},
   { path: 'search-video', component: SearchPageComponent },
   { path: 'settings', component: AccountSettingsComponent },
+  { path: 'subscription', component: SubscriptionComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
