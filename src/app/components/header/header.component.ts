05b3c241a9550dfa3e76e155170ee6deaa422b4e
Matthieu Nedir <matthieunedir@orange.fr>
2023-04-02 16:53:31 +0200

    modifs template - pret à être utilisé

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,8 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  leftButton: string;
+  rightButton: string;
 
-  constructor() { }
+  constructor() {
+    this.leftButton = `
+      <a class="button is-outlined is-white is-focused is-hovered has-text-black my-1 mx-2 p-1" routerLink="/menu">
+        <span class="icon">
+          <i class="fas fa-bars"></i>
+          leftbutton
+        </span>
+      </a>
+    `;
+    this.rightButton = `
+      <a class="button is-outlined is-link is-focused is-hovered has-text-white my-1 mx-2 p-1" routerLink="/settings">
+      <span class="icon">
+      <i class="fas fa-bars"></i>
+      rightbutton
+      </span>
+      </a>
+    `;
+  }
 
   ngOnInit(): void {
   }
