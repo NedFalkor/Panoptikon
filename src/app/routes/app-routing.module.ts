398994a176e391b4beb52cdd9cf7290822ab4691
Matthieu Nedir <matthieunedir@orange.fr>
2023-04-02 15:08:18 +0200

    ajout de routing

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AccountSettingsComponent } from '../components/account/account-settings/account-settings.component';
 import { MainMenuComponent } from '../components/main-menu/main-menu.component';
 import { SearchPageComponent } from '../components/search/search-page/search-page.component';
+import { AddUserComponent } from '../components/add/add-user/add-user.component';
+import { RegisterComponent } from '../components/register/register.component';
 
 
 const routes: Routes = [
+  { path: 'add-user', component: AddUserComponent},
   { path: 'menu', component: MainMenuComponent },
-  { path: 'search', component: SearchPageComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: 'search-video', component: SearchPageComponent },
   { path: 'settings', component: AccountSettingsComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
