e86e473ff78ca1e8ec814c4187dcc81691137cc8
NedFalkor <matthieunedir@orange.fr>
2023-04-09 14:27:56 +0200

    footer template done => ready to merge !

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,32 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+selector: 'app-footer',
+templateUrl: './footer.component.html',
+styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
-  leftButton: string;
-  rightButton: string;
+leftButton = { label: '', icon: '', color: '', route: '/' };
+rightButton = { label: '', icon: '', color: '', route: '/' };
 
-  constructor() {
-    this.leftButton = `
-      <a class="button is-outlined is-white is-focused is-hovered has-text-black my-1 mx-2 p-1" routerLink="/menu">
-        <span class="icon">
-          <i class="fas fa-bars"></i>
-        </span>
-        Menu
-      </a>
-    `;
-    this.rightButton = `
-      <a class="button is-outlined is-link is-focused is-hovered has-text-white my-1 mx-2 p-1" routerLink="/settings">
-        Paramètres
-      </a>
-    `;
-  }
+constructor() { }
 
-  ngOnInit(): void {
-  }
+ngOnInit(): void {
+  
+this.leftButton = { label: '', icon: '', color: '', route: '/' };
+this.rightButton = { label: '', icon: '', color: '', route: '/' };
+}
 
 }
