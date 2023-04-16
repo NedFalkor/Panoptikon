5b5bfd1a0ad26ca12bbe0688425a5719b0a91e60
Matthieu Nedir <matthieunedir@orange.fr>
2023-04-16 15:46:38 +0200

    subscription implement - mock

--- src/app/components/subscription/subscription.component.ts
+++ src/app/components/subscription/subscription.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-subscription',
@@ -8,11 +9,29 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class SubscriptionComponent implements OnInit {
 
+  user: User = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  errorMessage: string = '';
+
   constructor(
     private userService: UserService,
   ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSubmit() {
+    this.userService.createUser(this.user).subscribe(
+      user => {
+        console.log(user);
+      },
+      error => {
+        console.error(error);
+        this.errorMessage = 'An error occurred while subscribing. Please try again later.';
+      }
+    );
+  }
 }
--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,5 +1,6 @@
 export interface User {
     id?: number;
+    email?: string;
     username?: string;
     password?: string;
     grantType?: string;
