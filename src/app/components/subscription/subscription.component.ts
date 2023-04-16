1d099078f3e9ac2ffaa8902404dbda1b4a9887a6
Matthieu Nedir <matthieunedir@orange.fr>
2023-04-16 15:37:05 +0200

    implé services

--- src/app/components/subscription/subscription.component.ts
+++ src/app/components/subscription/subscription.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-subscription',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscriptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+  ) { }
 
   ngOnInit(): void {
   }
