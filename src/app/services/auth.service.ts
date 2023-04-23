5ee878af8ce558130bd38b2be78ebb861bebbe30
Matthieu Nedir <matthieunedir@orange.fr>
2023-04-23 09:58:50 +0200

    auth service register

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  register(user: User): Observable<User> {
+    const url = `${this.baseUrl}/users`;
+    return this.http.post<User>(url, user);
+  }
+
 }
