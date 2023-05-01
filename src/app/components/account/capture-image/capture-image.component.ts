c9dfec9fc78d8008da5c98c068a04d106605fecd
Matthieu Nedir <matthieunedir@orange.fr>
2023-05-01 13:44:00 +0200

    résolution capture image + ng if si photo

--- src/app/components/account/capture-image/capture-image.component.ts
+++ src/app/components/account/capture-image/capture-image.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WebcamImage } from 'ngx-webcam';
 import { Observable, Subject } from 'rxjs';
-import { WebcamModule } from 'ngx-webcam';
 
 @Component({
   selector: 'app-capture-image',
@@ -14,20 +13,27 @@ export class CaptureImageComponent implements OnInit {
   public webcamImage!: WebcamImage;
   private nextWebcam: Subject<any> = new Subject();
   sysImage = '';
+  isPhotoTaken = false;
+
   ngOnInit() {}
+
   public getSnapshot(): void {
     this.trigger.next(void 0);
+    this.isPhotoTaken = true;
   }
+
   public captureImg(webcamImage: WebcamImage): void {
     this.webcamImage = webcamImage;
     this.sysImage = webcamImage!.imageAsDataUrl;
     console.info('got webcam image', this.sysImage);
   }
+
   public get invokeObservable(): Observable<any> {
     return this.trigger.asObservable();
   }
+
   public get nextWebcamObservable(): Observable<any> {
     return this.nextWebcam.asObservable();
   }
-
 }
+
