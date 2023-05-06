c065f070514e6ac208b372db126cb7968f4c9b64
NedFalkor <matthieunedir@orange.fr>
2023-05-06 16:04:35 +0200

    MOdifs app video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GatekeeperModule } from './components/gatekeeper/gatekeeper.module';
+import { AddVideoComponent } from './components/add/add-video/add-video.component';
+import { AddModule } from './components/add/add.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AddVideoComponent
   ],
   imports: [
+    AddModule,
     GatekeeperModule,
     BrowserModule,
     AppRoutingModule,
--- src/app/components/add/add.module.ts
+++ src/app/components/add/add.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AddUserComponent } from './add-user/add-user.component';
 import { AddVideoComponent } from './add-video/add-video.component';
 
 @NgModule({
   declarations: [
   ],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
 export class AddModule { }
