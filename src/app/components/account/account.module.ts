1f922a120d1235f49b4705e3f843f19eb40d87df
NedFalkor <matthieunedir@orange.fr>
2023-06-02 10:06:26 +0200

    implémentation fonctionnelle

--- src/app/components/account/account.module.ts
+++ src/app/components/account/account.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
-
-
 
 @NgModule({
   declarations: [],
